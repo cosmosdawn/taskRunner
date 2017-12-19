c1a10fecb0f7d7dafda645fac6de6d5b38859491
xiaoyu <tornadowangxiaoyutornado@outlook.com>
2017-12-19 17:13:16 +0800

    注释规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
-};
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "commonjs": true,
+//         "es6": true,
+//         "node": true
+//     },
+//     "extends": "eslint:recommended",
+//     "parserOptions": {
+//         "ecmaFeatures": {
+//             "jsx": true
+//         },
+//         "sourceType": "module"
+//     },
+//     "rules": {
+//         "indent": [
+//             "error",
+//             "tab"
+//         ],
+//         "linebreak-style": [
+//             "error",
+//             "unix"
+//         ],
+//         "quotes": [
+//             "error",
+//             "single"
+//         ],
+//         "semi": [
+//             "error",
+//             "always"
+//         ]
+//     }
+// };
